54193f6fee796055ac4b25f67a6ac7b7260b65da
Sebastian <Sebastianharris85@gmail.com>
2026-07-15 11:24:07 +0200

    Add project page for Eddy with layout, styles, and navigation; implement footer

---
+++ componets/footer.js
@@ -0,0 +1,16 @@
+document.getElementById('footer-placeholder').innerHTML = `
+<footer>
+    <div class="footer-top">
+        <h2 class="footer-heading">LETS WORK TOGETHER</h2>
+        <p class="footer-body">Have a project in mind, or just want to say hi. I'm always happy to talk design.</p>
+    </div>
+    <div class="footer-bottom">
+        <p class="footer-copy">Copyright ©2026 All right Reserved Sebastian Harris Design</p>
+        <ul class="footer-links">
+        <li><a href="#">LinkedIn ↗</a></li>
+        <li><a href="#">Instagram ↗</a></li>
+        <li><a href="#">Email ↗</a></li>
+        </ul>
+    </div>
+</footer>
+`;
---
+++ componets/nav.js
@@ -0,0 +1,10 @@
+document.getElementById('nav-placeholder').innerHTML = `
+    <nav>
+    <a href="/index.html" class="logo">Sebastianhar</a>
+    <ul class="nav-links">
+        <li><a href="/index.html#about">About</a></li>
+        <li><a href="/index.html#projects">My work</a></li>
+        <li><a href="/index.html#contact" class="btn-nav">Get in Touch</a></li>
+    </ul>
+    </nav>
+`;
