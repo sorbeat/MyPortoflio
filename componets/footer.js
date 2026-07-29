b84d7167c40bb02765de7c137555901b12281e16
Sebastian <Sebastianharris85@gmail.com>
2026-07-29 10:16:58 +0200

    Refactor footer structure and update links in index.html

--- componets/footer.js
+++ componets/footer.js
@@ -4,13 +4,14 @@ document.getElementById('footer-placeholder').innerHTML = `
         <h2 class="footer-heading">LETS WORK TOGETHER</h2>
         <p class="footer-body">Have a project in mind, or just want to say hi. I'm always happy to talk design.</p>
     </div>
-    <div class="footer-bottom">
-        <p class="footer-copy">Copyright ©2026 All right Reserved Sebastian Harris Design</p>
-        <ul class="footer-links">
-        <li><a href="#">LinkedIn ↗</a></li>
-        <li><a href="#">Instagram ↗</a></li>
-        <li><a href="#">Email ↗</a></li>
-        </ul>
-    </div>
+    
+    <<div class="footer-bottom">
+    <p class="footer-copy">Copyright ©2026 All right Reserved Sebastian Harris Design</p>
+    <ul class="footer-links">
+        <li><a href="https://www.linkedin.com/in/sebastian-harris-9599502b4/" target="_blank" rel="noopener noreferrer">LinkedIn ↗</a></li>
+        <li><a href="#" target="_blank" rel="noopener noreferrer">Instagram ↗</a></li>
+        <li><a href="mailto:sebastianhar85@gmail.com">Email ↗</a></li>
+    </ul>
+</div>
 </footer>
 `;
